f27819165e15f7234905ce073eb2dc7c471253e7
Pravin Temghare <pravin.a.temghare@capgemini.com>
2021-09-02 13:50:49 +0530

    message change

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -9,7 +9,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello!!!";
+        return "Hello Pravin!!!";
     }
 
     public static void main(String[] args) throws Exception {
